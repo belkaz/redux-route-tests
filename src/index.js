f12388849dc41e4978322d7428365631142d91ac
belkaz <targetnlacc@gmail.com>
2019-03-28 17:52:11 +0300

    q2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
